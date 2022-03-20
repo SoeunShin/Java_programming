d4f8d15cc3767a3d2c901de36fad3ee6da185c59
Soeun Shin <61735948+SoeunShin@users.noreply.github.com>
2022-03-20 23:08:34 +0900

    HW1

---
+++ ShinSoEun.java
@@ -0,0 +1,8 @@
+//  21900395 신소은 
+public class ShinSoEun {
+	public static void main(String[] args) {
+		System.out.println("Name : ShinSoEun");
+		System.out.println("University : Handong Global University");
+		System.out.println("Student Number : 21900395");
+	}
+}
